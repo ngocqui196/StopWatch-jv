c9d11288fab9d8aaa0bd5922bf32c162fe7ce56b
ngocqui1996 <hivongmotloihua@yahoo.com>
2020-08-03 19:46:59 +0700

    sua lai

--- src/Display.java
+++ src/Display.java
@@ -1,2 +1,13 @@
+
 public class Display {
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        long sum = 0;
+        System.out.println(stopWatch.start());
+        for (long i = 0; i < 1000000000; i++) {
+            sum+=i;
+        }
+        System.out.println(stopWatch.stop());
+        System.out.println(stopWatch.getElapsedTime());
+    }
 }
